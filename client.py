9c1b317cdf9ec66dc2dff33311050f6cd5bcb613
Kevin Cui JY <kevincui1230@gmail.com>
2019-10-09 20:59:55 -0500

    Add DBL API

--- client.py
+++ client.py
@@ -568,5 +568,5 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-bot.run(token)
 setup(bot, api_token)
+bot.run(token)
--- imports/api/dblapi.py
+++ imports/api/dblapi.py
@@ -12,7 +12,7 @@ class DiscordBotsOrgAPI(commands.Cog):
     def __init__(self, bot, token):
         self.bot = bot
         self.token = token
-        self.dblpy = dbl.Client(self.bot, self.token)
+        self.dblpy = dbl.DBLClient(self.bot, self.token)
         self.updating = self.bot.loop.create_task(self.update_stats())
 
     async def update_stats(self):
@@ -24,7 +24,7 @@ class DiscordBotsOrgAPI(commands.Cog):
                 logger.info('Posted server count ({})'.format(self.dblpy.guild_count()))
             except Exception as e:
                 logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
-            await asyncio.sleep(1800)
+            await asyncio.sleep(10)
 
 def setup(bot, token):
     global logger
